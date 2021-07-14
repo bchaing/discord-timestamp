09f8f566cc65eff69ff63b7f9d2f3e85d0cdff4f
Brandon Chaing <bchaing@ucsd.edu>
2021-07-13 19:37:36 -0700

    Add dropdown time select and css font

--- script.js
+++ script.js
@@ -3,16 +3,24 @@ const date = new Date();
 const year = date.getFullYear();
 const month = String(date.getMonth() + 1).padStart(2, 0);
 const day = String(date.getDate()).padStart(2, 0);
-const hours = String(date.getHours()).padStart(2, 0);
-const mins = String(date.getMinutes()).padStart(2, 0);
-const secs = String(date.getSeconds()).padStart(2, 0);
+const hours = date.getHours() % 12;
+const mins = date.getMinutes();
+const secs = date.getSeconds();
+const ampm = date.getHours() - 12 <= 0 ? "AM" : "PM";
 
 // set current unix time
 const currUnix = document.getElementById("curr-unix");
 currUnix.innerText = Date.now();
 
 // form defaults
-const inDate = document.getElementById("date");
-const inTime = document.getElementById("time");
-inDate.value = `${year}-${month}-${day}`;
-inTime.value = `${hours}:${mins}:${secs}`;
+const selectDate = document.getElementById("date-select");
+const selectHour = document.getElementById("hour-select");
+const selectMin = document.getElementById("mins-select");
+const selectSec = document.getElementById("secs-select");
+const selectAMPM = document.getElementById("ampm-select");
+
+selectDate.value = `${year}-${month}-${day}`;
+selectHour.value = hours;
+selectMin.value = mins;
+selectSec.value = secs;
+selectAMPM.value = ampm;
